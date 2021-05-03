e276764ef7f8e15661198cd3d43b6f5d64ec95f0
David R. (Chip) Kent IV <chipkent@cecropiacapital.com>
2021-05-02 22:08:54 -0600

    Github action debugging.

--- deps/build.jl
+++ deps/build.jl
@@ -9,3 +9,10 @@ Conda.update()
 Conda.add(["google-api-python-client","google-auth-httplib2","google-auth-oauthlib"])
 
 println(Conda.list())
+using PyCall
+pickle = pyimport("pickle")
+os_path = pyimport("os.path")
+build = pyimport("googleapiclient.discovery").build
+InstalledAppFlow = pyimport("google_auth_oauthlib.flow").InstalledAppFlow
+Request = pyimport("google.auth.transport.requests").Request
+open = pybuiltin("open")
