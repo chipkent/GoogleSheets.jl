d73d63562c42b317790d3cf106b8ba99e830f044
David R. (Chip) Kent IV <chipkent@cecropiacapital.com>
2021-05-02 22:18:41 -0600

    Github action debuggin

--- deps/build.jl
+++ deps/build.jl
@@ -8,6 +8,7 @@ Conda.add_channel("conda-forge")
 Conda.update()
 Conda.add(["google-api-python-client","google-auth-httplib2","google-auth-oauthlib"])
 
+println("ENV: ", env)
 println(Conda.list())
 using PyCall
 pickle = pyimport("pickle")
@@ -16,3 +17,4 @@ build = pyimport("googleapiclient.discovery").build
 InstalledAppFlow = pyimport("google_auth_oauthlib.flow").InstalledAppFlow
 Request = pyimport("google.auth.transport.requests").Request
 open = pybuiltin("open")
+println("EVERYTHING WORKED")
