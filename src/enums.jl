81c97787a70065cba75761869ff8f10029731c77
David R. (Chip) Kent IV <chipkent@cecropiacapital.com>
2021-05-06 23:48:10 -0600

    Docs debugging

--- src/enums.jl
+++ src/enums.jl
@@ -97,6 +97,11 @@ A Google Sheets number format type.
 
 NUMBER_FORMAT_TYPE_TEXT: text formatting, e.g. 1000.12
 NUMBER_FORMAT_TYPE_NUMBER: number formatting, e.g. 1,000.12
-NUMBER_FORMAT_TYPE_SCIENTIFIC: scientific number formatting, e.g 1.01E+03
+NUMBER_FORMAT_TYPE_PERCENT: percent formatting, e.g. 10.12%
+NUMBER_FORMAT_TYPE_CURRENCY: currency formatting, e.g. \$1,000.12
+NUMBER_FORMAT_TYPE_DATE: date formatting, e.g. 9/26/2008
+NUMBER_FORMAT_TYPE_TIME: time formatting, e.g. 3:59:00 PM
+NUMBER_FORMAT_TYPE_DATE_TIME: date+time formatting, e.g. 9/26/08 15:59:00
+NUMBER_FORMAT_TYPE_SCIENTIFIC: scientific number formatting, e.g. 1.01E+03
 """
 @_gsheet_enum NumberFormatType NUMBER_FORMAT_TYPE_ TEXT NUMBER PERCENT CURRENCY DATE TIME DATE_TIME SCIENTIFIC
