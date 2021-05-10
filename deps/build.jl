b15bfe69fd3488f56222daac1c6f98b01bb0ae9c
David R. (Chip) Kent IV <chipkent@cecropiacapital.com>
2021-05-09 22:19:00 -0600

    Fixed build dependencies

--- deps/build.jl
+++ deps/build.jl
@@ -2,6 +2,8 @@
 # To run this script:
 # pkg> build GoogleSheets
 
+println("Instalation of GoogleSheets python package dependencies")
+
 using PyCall
 @pyimport pip
-pip.main(["install","google-api-python-client","google-auth-httplib2","google-auth-oauthlib","FAIL-HARD"])
+pip.main(["install","google-api-python-client","google-auth-httplib2","google-auth-oauthlib"])
